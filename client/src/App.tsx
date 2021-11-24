896ac1a630aee889ee6bf6aedaf45105978849cf
victorchu8 <victorchu1996@gmail.com>
2021-11-24 11:39:34 -0700

    Add react-router-dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,15 +4,16 @@ import LoginForm from "./components/login/LoginForm";
 import NavBar from "./components/navigation/NavBar";
 import SignUpForm from "./components/signup/SignUpForm";
 
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
+      <NavBar />
+      <LoginForm />
       <Routes>
-        <Route path = '/login' element = {<LoginForm />} />
-        <Route path = '/signup' element={<SignUpForm/>} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/signup" element={<SignUpForm />} />
       </Routes>
     </div>
   );
