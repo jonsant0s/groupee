7cd67464d077686b177a77ddfb5b4b0f04c7e2f8
Jonathan <jonsant0s@outlook.com>
2021-11-24 10:20:05 -0700

    Updated App.tsx to include routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,19 @@
 import React from "react";
 import "./App.css";
-
-import { BrowserRouter as Route, Routes, Router } from 'react-router-dom';
-
 import LoginForm from "./components/login/LoginForm";
 import NavBar from "./components/navigation/NavBar";
 import SignUpForm from "./components/signup/SignUpForm";
 
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <Routes>
-        <Route path="/login" element={<LoginForm/>} />
-        <Route path="/signup" element={<SignUpForm/>} />
+        <Route path = '/login' element = {<LoginForm />} />
+        <Route path = '/signup' element={<SignUpForm/>} />
       </Routes>
-
     </div>
   );
 }
