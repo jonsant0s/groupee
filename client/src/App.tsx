688651b473ce4064de6f63af10f2d8b655624ae6
Victor Chu <victorchu1996@gmail.com>
2021-11-23 21:53:35 -0700

    Create initial login screen (#3)
    * Create initial login screen
    
    * Mistake in naming
    
    * Change email fields to username
    
    Co-authored-by: Jonathan Santos <52723949+jonsant0s@users.noreply.github.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import "./App.css";
+import LoginForm from "./components/login/LoginForm";
+
 import SignUpForm from "./components/signup/SignUpForm";
+
 
 function App() {
   return (
     <div className="App">
+      <LoginForm />
+
       <SignUpForm />
+
     </div>
   );
 }
---
+++ client/src/components/login/LoginForm.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Button from "react-bootstrap/esm/Button";
+import Form from "react-bootstrap/esm/Form";
+import "./LoginForm.css";
+
+export default function LoginForm() {
+  return (
+    <div className="containerDiv">
+      <div className="titleDiv">
+        <h1>Groupee</h1>
+        <h6>School Group Managment System</h6>
+      </div>
+      <div>
+        <Form className="form">
+          <Form.Group className="usernameGroup" controlId="formBasicUsername">
+            <div>
+              <Form.Label className="usernameLabe">Username</Form.Label>
+            </div>
+            <Form.Control type="username" />
+          </Form.Group>
+          <Form.Group className="passwordGroup" controlId="formBasicPassword">
+            <div>
+              <Form.Label>Password</Form.Label>
+            </div>
+            <Form.Control type="password" />
+          </Form.Group>
+          <Button className="submitButton" variant="primary" type="submit">
+            Login
+          </Button>
+        </Form>
+      </div>
+    </div>
+  );
+}
