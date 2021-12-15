70bae954430b514471c682000f8c4248af096756
Kyla <60671350+kmemps@users.noreply.github.com>
2021-12-15 12:30:07 -0700

    Update app requestform route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,37 @@
 import { Route, Routes } from "react-router-dom";
 
 import { HomeScreen } from "./components/home";
-import { RequestForm, RequestList } from "./components/request";
+import { RequestList } from "./components/request";
 import { LoginForm, SignUpForm } from "./components/auth";
 import { NavBar } from "./components/navigation/NavBar";
 import { Forum } from "components/forum";
 
 import "./App.css";
+import axios from "axios";
+import ClasslistScreen from "components/classlist/ClasslistScreen";
+import { useEffect } from "react";
 
 const App = () => {
+    useEffect(() => {
+        axios.get("http://localhost:3001/")
+            .then(() => {
+                return axios.get("http://localhost:3001/populate");
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    },[]);
+
     return (
         <div className="App">
             <NavBar />
             <Routes>
-                <Route path="/" element={<HomeScreen />} />
+                <Route path="/home" element={<HomeScreen />} />
                 <Route path="/login" element={<LoginForm />} />
                 <Route path="/signup" element={<SignUpForm />} />
-                <Route path="/forum" element={<Forum />} />
                 <Route path="/requestlist" element={<RequestList />} />
+                <Route path="/classlist" element={<ClasslistScreen />} />
+                <Route path="/forum" element={<Forum />} />
             </Routes>
         </div>
     );
